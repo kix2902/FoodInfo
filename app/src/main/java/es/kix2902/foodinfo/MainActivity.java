f7176ecb6c570c17c5baafe4f1a336c93c9463fe
kix2902 <ismael.kix2902@gmail.com>
2015-09-02 21:36:32 +0200

    Added 2 fab-mini when pressed FabSearch Added necessary drawables

--- app/src/main/java/es/kix2902/foodinfo/MainActivity.java
+++ app/src/main/java/es/kix2902/foodinfo/MainActivity.java
@@ -1,18 +1,55 @@
 package es.kix2902.foodinfo;
 
 import android.os.Bundle;
-import android.os.PersistableBundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Boolean expandedFab = false;
+    private FloatingActionButton fabBarcode, fabName, fabSearch;
 
     @Override
-    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        fabSearch = (FloatingActionButton) findViewById(R.id.fabSearch);
+        fabSearch.setOnClickListener(this);
+
+        fabBarcode = (FloatingActionButton) findViewById(R.id.fabBarcode);
+        fabName = (FloatingActionButton) findViewById(R.id.fabName);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.fabSearch:
+                if (expandedFab) {
+                    collapseFab();
+                } else {
+                    expandFab();
+                }
+                break;
+        }
+    }
+
+    private void expandFab() {
+        fabBarcode.setVisibility(View.VISIBLE);
+        fabName.setVisibility(View.VISIBLE);
+        fabSearch.setImageResource(R.drawable.ic_close);
+        expandedFab = true;
+    }
+
+    private void collapseFab() {
+        fabBarcode.setVisibility(View.GONE);
+        fabName.setVisibility(View.GONE);
+        fabSearch.setImageResource(R.drawable.ic_search);
+        expandedFab = false;
     }
 }
